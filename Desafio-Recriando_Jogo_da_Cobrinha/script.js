fb9a12e39974fbcde0cc758d77428b4266e0af3b
Guilherme Marques <guilherme013m@gmail.com>
2020-07-01 16:51:40 -0300

    Definido fim do jogo

--- Desafio-Recriando_Jogo_da_Cobrinha/script.js
+++ Desafio-Recriando_Jogo_da_Cobrinha/script.js
@@ -46,6 +46,13 @@ function iniciarJogo(){
     if(snake[0].y > 15 * box && direction == "down") snake[0].y  = 0;
     if(snake[0].y < 0 && direction =="up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert("Game Over  :(")
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -64,7 +71,7 @@ function iniciarJogo(){
         food.x = Math.floor(Math.random() * 15 + 1) * box;
         food.y = Math.floor(Math.random() * 15 + 1) * box;
     }
-    
+
     let newHead = {
         x: snakeX,
         y: snakeY
